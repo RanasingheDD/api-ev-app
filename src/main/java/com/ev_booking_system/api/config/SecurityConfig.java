a606272f82587e39eabfd16827256c60576bddb1
Dishan Ranasinghe <162302275+RanasingheDD@users.noreply.github.com>
2026-01-05 13:03:40 +0530

    Update SecurityConfig.java

--- src/main/java/com/ev_booking_system/api/config/SecurityConfig.java
+++ src/main/java/com/ev_booking_system/api/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ev_booking_system.api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,13 +12,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()       // disable CSRF for API
+            .cors()
+        .and()
+        .csrf(csrf -> csrf.disable()) // Disable CSRF for APIs
             .authorizeHttpRequests()
-                .anyRequest().permitAll()                // protect other endpoints
+            .anyRequest().permitAll()                // protect other endpoints
             .and()
             .httpBasic().disable()    // disable basic auth
             .formLogin().disable();   // disable default login page
 
         return http.build();
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
